a7f2fb1168f44f4e4e979deaa14a34dface534a0
peter oluwaseyi <peterseyi1010@gmail.com>
2025-08-29 15:38:00 +0100

    fixed mint nft page

--- src/Pages/NftMinting.tsx
+++ src/Pages/NftMinting.tsx
@@ -1,10 +1,5 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import {
-  Card,
-  CardContent,
-  CardFooter,
-} from "@/components/ui/card";
 import StacksplaysPunk from "/public/StacksplaysPunk.png";
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
@@ -27,51 +22,54 @@ export const NftMinting = () => {
     <>
       <Header />
       <div className="flex flex-col justify-center items-center min-h-screen m-24">
-        <div className="flex justify-center items-center w-full ml-20 mr-20 bg-foreground/10 backdrop-blur-sm p-6 rounded-lg mb-18">
+        <div className="flex justify-center items-center w-full m-12 bg-foreground/10 backdrop-blur-sm p-6 rounded-lg">
           <h1 className="text-primary text-3xl md:text-6xl max-w-sm tracking-tighter font-semi-bold">
             Mint NFT
           </h1>
         </div>
-        <Card className="mb-8">
-          <CardContent className="flex justify-center items-center">
-            <img
-              src={StacksplaysPunk}
-              alt="StacksPlays Punk"
-              className="w-120 h-120 object-contain rounded-md"
-            />
-          </CardContent>
-          <CardFooter className="flex justify-center items-center">
-            <h1 className="text-primary text-2xl md:text-4xl max-w-sm tracking-tighter font-bold">STACKSPLAYS PUNKS</h1>
-          </CardFooter>
-        </Card>
-        {/* Mint NFT Button and Quantity Section */}
-        <div className="flex flex-col items-center gap-4">
-          {/* Quantity Control */}
-          <div className="flex items-center gap-6">
-            <Button
-              className="p-8 text-2xl font-bold cursor-pointer" variant={"outline"} size={"lg"}
-              onClick={decrement}
-            >
-              -
-            </Button>
-            <div>
-              <span className="text-2xl font-bold">{quantity}</span>
+        <div className=" justify-top items-top gap-12">
+          <div className="mb-8 bg-card border-2 border-secondary rounded-xl p-8">
+            <div className="flex justify-center items-center mb-8">
+              <img
+                src={StacksplaysPunk}
+                alt="StacksPlays Punk"
+                className="w-120 h-120 object-contain rounded-md"
+              />
             </div>
+            <div className="flex justify-center items-center">
+              <h1 className="text-primary text-2xl md:text-4xl max-w-sm tracking-tighter font-bold">STACKSPLAYS PUNKS</h1>
+            </div>
+          </div>
+          {/* Mint NFT Button and Quantity Section */}
+          <div className="flex flex-col items-center gap-4">
+            {/* Quantity Control */}
+            <div className="flex items-center justify-between w-full">
+              <Button
+                className="p-8 text-2xl font-bold cursor-pointer rounded-md" variant={"outline"} size={"lg"}
+                onClick={decrement}
+              >
+                -
+              </Button>
+              <div className="flex flex-1 mx-2 justify-center items-center rounded-md p-4 border-1">
+                <span className="text-2xl font-bold">{quantity}</span>
+              </div>
+              <Button
+                className="p-8 text-2xl font-bold cursor-pointer rounded-md" variant={"outline"} size={"lg"}
+                onClick={increment}
+              >
+                +
+              </Button>
+            </div>
+
+            {/* Mint NFT Button */}
             <Button
-              className="p-8 text-2xl font-bold cursor-pointer" variant={"outline"} size={"lg"}
-              onClick={increment}
+              className="w-full rounded-md text-2xl font-semi-bold p-8 cursor-pointer" size={"lg"}
             >
-              +
+              Mint NFT
             </Button>
           </div>
-
-          {/* Mint NFT Button */}
-          <Button
-            className="w-full rounded-sm text-2xl font-semi-bold p-6 cursor-pointer" size={"lg"}
-          >
-            Mint NFT
-          </Button>
         </div>
+
       </div>
 
       <Footer />
